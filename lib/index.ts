9419d6a4ab726b09919ed42e9da0ea540781f08a
Steve Thompson <writetome51@gmail.com>
2020-07-05 19:20:28 -0600

    Updated implementation to reflect changes in other supporting classes

--- lib/index.ts
+++ lib/index.ts
@@ -3,8 +3,7 @@
  all the data to be paginated can't be loaded in memory at once.  Instead  
  of only requesting one page of data at-a-time from the data source, the  
  paginator has the option of requesting a bigger load, determined by the  
- property `itemsPerLoad`.  This makes the paginator's communication  
- with the data source more efficient.
+ property `itemsPerLoad`.
 
  Though it is a class, most of its implementation does not exist as-is.  A  
  subclass must be made, which provides a `dataSource` and  
@@ -23,20 +22,13 @@ export abstract class AbstractBigDatasetPaginator {
 
 	// These 3 properties must be assigned values inside `this.__setup()` (see constructor).
 
-	private __pageInfo: { itemsPerPage: number, totalPages: number };
-	private __loadInfo: { itemsPerLoad: number };
+	private __pageInfo: {
+		setItemsPerPage: (num) => void, getItemsPerPage: () => number, getTotalPages: () => number
+	};
+	private __loadInfo: { setItemsPerLoad: (num) => void, getItemsPerLoad: () => number };
 
-	private __pageLoader: {
-
-		loadPage: (pageNumber) => Promise<void>,
-
-		// Must load `pageNumber` all over again, even if that page is already currently loaded.
-
-		forceLoadPage: (pageNumber) => Promise<void>,
-
-		// All items in the loaded page.
-
-		loadedPage: any[]
+	private __currentPage: {
+		get: () => any[], set: (pageNumber) => Promise<void>, reset: (pageNumber) => Promise<void>
 	};
 
 
@@ -48,46 +40,29 @@ export abstract class AbstractBigDatasetPaginator {
 	// Total number of items the app can have loaded in memory.  Set this to highest number that
 	// does not negatively affect app performance.
 
-	set itemsPerLoad(value) {
-		this.__loadInfo.itemsPerLoad = value;
+	setItemsPerLoad(num) {
+		this.__loadInfo.setItemsPerLoad(num);
 	}
 
 
-	get itemsPerLoad(): number {
-		return this.__loadInfo.itemsPerLoad;
+	getItemsPerLoad(): number {
+		return this.__loadInfo.getItemsPerLoad();
 	}
 
 
-	set itemsPerPage(value) {
-		this.__pageInfo.itemsPerPage = value;
+	setItemsPerPage(num) {
+		this.__pageInfo.setItemsPerPage(num);
 	}
 
 
-	get itemsPerPage(): number {
-		return this.__pageInfo.itemsPerPage;
+	getItemsPerPage(): number {
+		return this.__pageInfo.getItemsPerPage();
 	}
 
 
-	get currentPageNumber(): number {
-		return this.__currentPageNumber;
-	}
-
-
-	get currentPage(): any[] {
-		return this.__pageLoader.loadedPage;
-	}
-
-
-	get totalPages(): number {
-		return this.__pageInfo.totalPages;
-	}
-
-
-	// Updates this.currentPage
-
-	async set_currentPageNumber(value): Promise<void> {
-		this.__currentPageNumber = value;
-		await this.__pageLoader.loadPage(value);
+	async setCurrentPageNumber(num): Promise<void> {
+		this.__currentPageNumber = num;
+		await this.__currentPage.set(num);
 	}
 
 
@@ -96,7 +71,22 @@ export abstract class AbstractBigDatasetPaginator {
 
 	async resetToFirstPage(): Promise<void> {
 		this.__currentPageNumber = 1;
-		await this.__pageLoader.forceLoadPage(1);
+		await this.__currentPage.reset(1);
+	}
+
+
+	getCurrentPageNumber(): number {
+		return this.__currentPageNumber;
+	}
+
+
+	getCurrentPage(): any[] {
+		return this.__currentPage.get();
+	}
+
+
+	getTotalPages(): number {
+		return this.__pageInfo.getTotalPages();
 	}
 
 
