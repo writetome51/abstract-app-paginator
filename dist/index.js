8d212941b04503e4a5c360944f73ac050414fcd7
Steve Thompson <writetome51@gmail.com>
2021-03-10 11:19:13 -0700

    Updated to mirror changes in lib/index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,21 @@
 /***************************
- An abstract TypeScript/Javascript class intended for pagination where
- all the data to be paginated can't be loaded in memory at once.  Instead
- of only requesting one page of data at-a-time from the data source, the
- paginator has the option of requesting multiple pages of data to make
- requests more efficient.  You configure this with the functions
- `setItemsPerPage()` and `setItemsPerLoad()`. (A load is either the total
- number of items you want the app to have in memory at once, or the total
- number of items your data source is willing to give you at once —— whichever
- is less.)
+ An abstract TypeScript/Javascript class intended for pagination where all the data to be
+ paginated can't be loaded in memory at once.  Instead of only requesting one page of data
+ at-a-time from the data source, the paginator has the option of requesting multiple pages
+ of data to make requests more efficient.  You configure this with the functions
+ `setItemsPerPage()` and `setItemsPerLoad()`. (A load is either the total number of items
+ you want the app to have in memory at once, or the total number of items your data source
+ is willing to give you at once —— whichever is less.)
 
- A subclass must pass a `__setup()` function to this class' constructor
- (`__setup()` becomes a private method to give it access to this class'
- private properties).  As for what `__setup()` does, the only requirement is
- the properties `__pageInfo`,`__loadInfo`, and `__currentPage` must be
- assigned values inside it so the code here will execute.
+ A subclass must pass a `__setup()` function to this class' constructor (`__setup()`
+ becomes a private method to give it access to this class' private properties).  The only
+ requirement for `__setup()` is the properties `__pageInfo`, `__loadInfo`, and
+ `__loadedPage` must be assigned values inside it.
  ***************************/
 
 export class AbstractBigDatasetPaginator {
 
-	constructor(
-		__setup,
-		setupArgs = []
-	) {
+	constructor(__setup, setupArgs = []) {
 		this.__setup = __setup;
 		this.__setup(...setupArgs);
 	}
@@ -47,31 +41,26 @@ export class AbstractBigDatasetPaginator {
 	}
 
 
-	async setCurrentPageNumber(num) {
-		await this.__currentPage.set(num);
+	getTotalPages() {
+		return this.__pageInfo.getTotalPages();
+	}
+
+
+	// Set `option.reload` to true if page data must be reloaded from the source
+
+	async setCurrentPageNumber(num, option = {reload: false}) {
+		if (option.reload) await this.__loadedPage.reset(num);
+		else await this.__loadedPage.set(num);
 	}
 
 
 	getCurrentPageNumber() {
-		return this.__currentPage.getNumber();
-	}
-
-
-	// Intended to be called after the order of the dataset changes (like after sorting),
-	// or after the total number of items changes (like after a search).
-
-	async resetToFirstPage() {
-		await this.__currentPage.reset(1);
+		return this.__loadedPage.getNumber();
 	}
 
 
 	getCurrentPage() {
-		return this.__currentPage.get();
-	}
-
-
-	getTotalPages() {
-		return this.__pageInfo.getTotalPages();
+		return this.__loadedPage.get();
 	}
 
 }
